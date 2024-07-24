b01846ca45cbebaa0f72eb7df71ea20aee5363ae
Md Afiq Iskandar <mdafiqiskandar@gmail.com>
2024-07-24 13:41:38 +0800

    Fix nav

--- navigation/index.js
+++ navigation/index.js
@@ -1,107 +1,104 @@
 export default [
   {
-    "header": "",
-    "description": "",
-    "child": [
+    header: "",
+    description: "",
+    child: [
       {
-        "title": "Dashboard",
-        "path": "/dashboard",
-        "icon": "ic:outline-dashboard",
-        "child": [],
-        "meta": {}
-      }
-    ]
-  },
-  {
-    "header": "Administration",
-    "description": "Manage your application",
-    "child": [
-      {
-        "title": "Configuration",
-        "icon": "ic:outline-settings",
-        "child": [
-          {
-            "title": "Environment",
-            "path": "/devtool/config/environment"
-          }
-        ]
-      },
-      {
-        "title": "Menu Editor",
-        "icon": "ci:menu-alt-03",
-        "path": "/devtool/menu-editor",
-        "child": []
-      },
-      {
-        "title": "Manage Users",
-        "path": "/devtool/user-management",
-        "icon": "ph:user-circle-gear",
-        "child": [
-          {
-            "title": "User List",
-            "path": "/devtool/user-management/user-list",
-            "icon": "",
-            "child": []
-          },
-          {
-            "title": "Role List",
-            "path": "/devtool/user-management/role-list",
-            "icon": "",
-            "child": []
-          }
-        ]
-      },
-      {
-        "title": "Content",
-        "icon": "mdi:pencil-ruler",
-        "child": [
-          {
-            "title": "Editor",
-            "path": "/devtool/content-editor"
-          },
-          {
-            "title": "Template",
-            "path": "/devtool/content-editor/template"
-          }
-        ]
-      },
-      {
-        "title": "API Editor",
-        "path": "/devtool/api-editor",
-        "icon": "material-symbols:api-rounded",
-        "child": []
+        title: "Dashboard",
+        path: "/dashboard",
+        icon: "ic:outline-dashboard",
+        child: [],
+        meta: {},
       },
     ],
-    "meta": {
-      "auth": {
-        "role": [
-          "Developer"
-        ]
-      }
-    }
   },
   {
-    "header": "Help",
-    "description": "Help and documentation",
-    "child": [
+    header: "Administration",
+    description: "Manage your application",
+    child: [
       {
-        "title": "Documentation",
-        "icon": "solar:book-bookmark-minimalistic-bold",
-        "path": "https://mawar-cms-docs.vercel.app",
-        "child": []
+        title: "Configuration",
+        icon: "ic:outline-settings",
+        child: [
+          {
+            title: "Environment",
+            path: "/devtool/config/environment",
+          },
+        ],
       },
       {
-        "title": "UI Components",
-        "icon": "material-symbols:settings-input-component-outline-rounded",
-        "path": "https://corradui.datasc.dev"
-      }
+        title: "Menu Editor",
+        icon: "ci:menu-alt-03",
+        path: "/devtool/menu-editor",
+        child: [],
+      },
+      {
+        title: "Manage Users",
+        path: "/devtool/user-management",
+        icon: "ph:user-circle-gear",
+        child: [
+          {
+            title: "User List",
+            path: "/devtool/user-management/user-list",
+            icon: "",
+            child: [],
+          },
+          {
+            title: "Role List",
+            path: "/devtool/user-management/role-list",
+            icon: "",
+            child: [],
+          },
+        ],
+      },
+      {
+        title: "Content",
+        icon: "mdi:pencil-ruler",
+        child: [
+          {
+            title: "Editor",
+            path: "/devtool/content-editor",
+          },
+          {
+            title: "Template",
+            path: "/devtool/content-editor/template",
+          },
+        ],
+      },
+      {
+        title: "API Editor",
+        path: "/devtool/api-editor",
+        icon: "material-symbols:api-rounded",
+        child: [],
+      },
     ],
-    "meta": {
-      "auth": {
-        "role": [
-          "Developer"
-        ]
-      }
-    }
-  }
-]
+    meta: {
+      auth: {
+        role: ["Developer"],
+      },
+    },
+  },
+  {
+    header: "Help",
+    description: "Help and documentation",
+    child: [
+      {
+        title: "Documentation",
+        icon: "solar:book-bookmark-minimalistic-bold",
+        path: "https://mawar-cms-docs.vercel.app",
+        external: true,
+      },
+      {
+        title: "UI Components",
+        icon: "material-symbols:settings-input-component-outline-rounded",
+        path: "https://corradui.datasc.dev",
+        external: true,
+      },
+    ],
+    meta: {
+      auth: {
+        role: ["Developer"],
+      },
+    },
+  },
+];
