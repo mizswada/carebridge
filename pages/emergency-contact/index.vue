8e10d19f2b5afac17ca1afc16104a2a4e1afcaf7
zatytombark <izzaty@tombark.my>
2024-10-11 14:25:28 +0800

    new update on navigation

---
+++ pages/emergency-contact/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+definePageMeta({
+  title: "Emergency Contact",
+  middleware: ["auth"],
+  requiresAuth: true,
+});
+</script>
+
+<template>
+  <div>Emergency Contact</div>
+</template>
